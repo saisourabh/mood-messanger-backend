1e2607599bba5bcdf43d53ab8ade4c9f2086e664
saisourabh <sai.sourabh2@gmail.com>
2025-09-19 00:25:10 +0530

    New begining

--- src/main/java/messenger/mood/Mood/Messenger/MoodMessengerApplication.java
+++ src/main/java/messenger/mood/Mood/Messenger/MoodMessengerApplication.java
@@ -8,6 +8,7 @@ public class MoodMessengerApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Mood Messenger Starting");
+		System.out.println("New Begining");
 		SpringApplication.run(MoodMessengerApplication.class, args);
 	}
 
